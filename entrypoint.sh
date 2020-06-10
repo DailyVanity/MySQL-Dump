359daf9a7f164eb3b8acd6cae04d6ac067f4c215
Chris Sim <monkeymon@gmail.com>
2020-06-10 21:48:02 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,13 @@ DB_PORT="${INPUT_DATABASE_PORT:-3306}"
 DUMP_PATH="${INPUT_DUMPFILE_PATH:-backup}"
 TIME=`/bin/date +%d-%m-%Y-%T`
 
-DEFAULT_DUMPFILE=$DB_NAME.sql.gz
-DUMP_FILE="${INPUT_DUMPFILE_PATH:-$DEFAULT_DUMPFILE}"
+DEFAULT_DUMPFILE="${INPUT_DUMPFILE_PATH:-$DB_NAME}"
+DUMP_FILE="$DEFAULT_DUMPFILE.sql.tar.gz"
 
 mkdir -p $DUMP_PATH
 cd $DUMP_PATH
 
 echo "Dumping database now."
+echo "Backing dump to $DUMP_PATH/$DUMP_FILE"
 mysqldump -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD --single-transaction=TRUE $DB_NAME | pv --progress --size 100m | gzip > ./$DUMP_FILE
 echo "Complete"
